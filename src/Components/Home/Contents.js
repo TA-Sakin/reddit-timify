71d9d028feca56cccf81e701f9131a632e8f4529
TA-Sakin <tasakin98@gmail.com>
2022-06-27 00:21:37 +0600

    latest change

--- src/Components/Home/Contents.js
+++ src/Components/Home/Contents.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import table from "../../Assets/table.png";
 const Contents = () => {
   return (
     <div className="text-center mt-7">
@@ -8,10 +8,13 @@ const Contents = () => {
         Great timing, great results! Find the best time to post on your
         subreddit.
       </p>
-      <button className="py-2 px-5 font-medium mt-11 bg-orange-400 text-white rounded-sm hover:bg-orange-500 focus:outline-none focus:ring-opacity-75">
+      <button className="py-2 px-5 font-semibold mt-12 bg-orange-400 text-white rounded-sm hover:bg-orange-500 focus:outline-none focus:ring-opacity-75 text-sm">
         SHOW ME THE BEST TIME
       </button>
       <p className="text-[#93918F] mt-8">r/javascript</p>
+      <div className="flex justify-center mt-10">
+        <img src={table} alt="" />
+      </div>
     </div>
   );
 };

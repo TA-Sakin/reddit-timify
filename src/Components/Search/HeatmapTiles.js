27243edfb2af3e6fceae37f2bb367522d46d4f2a
TA-Sakin <tasakin98@gmail.com>
2022-07-15 02:16:51 +0600

    active tile added

--- src/Components/Search/HeatmapTiles.js
+++ src/Components/Search/HeatmapTiles.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { colors } from "../Shared/Constant";
 import PropTypes from "prop-types";
-
+import "./tiles.css";
 const HeatmapTiles = ({ posts, index, selected, setSelected }) => {
   let count = posts.length;
   const color = count >= 10 ? colors[10] : colors[count];
+  const handleClick = () => {
+    setSelected(index);
+  };
+
   return (
-    <div
-      style={{ backgroundColor: `${color}` }}
-      className={`h-10 w-10 box-border text-gray-700 cursor-pointer flex justify-center items-center text-[14px] font-bold hover:border-[1px] border-solid border-black`}
-    >
-      {posts.length}
-    </div>
+    <>
+      <div
+        onClick={handleClick}
+        active={selected}
+        style={{
+          backgroundColor: `${color}`,
+        }}
+        className={`${
+          selected && "m-active"
+        } h-10 w-10 box-border text-gray-700 cursor-pointer flex justify-center items-center text-[14px] font-bold hover:border-[1px] border-solid border-black `}
+      >
+        {posts.length}
+      </div>
+    </>
   );
 };
 HeatmapTiles.propTypes = {
